9606623df034d8683a20e19b094dc78aec91a8a0
locnht6 <locnht6@gmail.com>
2026-06-07 18:26:09 +0700

    feat: add HelloJava project in Session01-Hello

---
+++ Session01-Hello/HelloJava/src/hellojava/HelloJava.java
@@ -0,0 +1,14 @@
+package hellojava;
+
+public class HelloJava {
+
+    // Entry point, cửa chính, nơi cuộc chơi bắt đầu, CPU vào đây chạy lệnh.
+    public static void main(String[] args) {
+        System.out.println("Hello Java from Locnht6 with love <3");
+        // ~~~ printf("Hello Java...\n");
+        
+        System.out.println("5 + 5 = " + 5 + 5);
+        System.out.println("5 + 5 = " + (5 + 5));
+    }
+    
+}
