f079d89f22eb9ad747eaaa4df3341a814b7f3a2e
locnht6 <locnht6@gmail.com>
2026-06-11 08:29:17 +0700

    feat: update DogManagement project in Session03-Class

--- Session03-Class/DogManagement/src/data/Dog.java
+++ Session03-Class/DogManagement/src/data/Dog.java
@@ -6,6 +6,7 @@ package data;
 // CaSi là Khuôn chứa các info mô tả mọi object đi hát,
 // TEMPLATE, FORM, ...
 // Khuôn thì phải kín, code phải viết trong class.
+// Khuôn là hình dáng của 1 object sẽ xuất hiện trong tương lai, chứa đựng đặc tính/đặc điểm và hành vi (xử lí info - hàm).
 
 // Nhóm các bạn Dog chia sẻ chung nhiều đặc điểm: tên:___; cân nặng:___;
 public class Dog {
@@ -15,9 +16,28 @@ public class Dog {
     
     // Phếu dùng để rót/chế/đổ/fill vật liệu đưa vào, lấp đầy các chi tiết chừa sẵn chỗ bên trong.
     // Khuôn rỗng ở các chi tiết.
+    // Phễu là 1 hàm rất đặc biệt, nó xử lí: ép dòng dữ liệu/vật liệu chảy vào đúng chỗ bên trong - có xử lí - hàm theo quan điểm của lập trình.
+    // Mỗi lần gọi phễu, sử dụng phễu tương đương việc đúc 1 bức tượng mới, tạo 1 object mới.
+    // Lưu ý hàm này tên phải trùng 100% tên class, chữ hoa từng đầu từ - quy ước.
+    // Tên hàm phễu không có giá trị trả về, không được có VOID, vì VOID sẽ nhầm lẫn với các ứng xử/hành động của object.
+    // Hàm phễu nhận vật liệu vào, chờ khô, ra được chính object thuộc nhóm Dog đang đề cập,
+    // trả về ngay object sẽ được tạo trong tương lai, đúng không gian mình đang đứng đã là object,
+    // không cần giá trị trả về, thậm chí không được dùng void,
+    // trong khi đó nhờ phễu ra chính object này.
     public Dog(String iName, int iYob, double iWeight) {
         name= iName;
         yob = iYob;
         weight = iWeight;
     }
+    
+    // sủa() là hành động của object Dog.
+    // speak() là hành động của Person.
+    // scanChannel() là hành động của Tivi, showMovie().
+    public void bark() {
+//        System.out.println("gogogo... my name is " + name);
+//        System.out.println("gogogo... my yob is " + yob);  
+
+        System.out.printf("|GOGO|%-10s|%4d|%4.1f|\n", name, yob, weight);
+    }
+    
 }
--- Session03-Class/DogManagement/src/dogmanagement/DogManagement.java
+++ Session03-Class/DogManagement/src/dogmanagement/DogManagement.java
@@ -1,9 +1,33 @@
 package dogmanagement;
 
+import data.Dog; // đi mượn cái Khuôn Dog ở ngăn data về đúc
+                 // ~ #include <stdio.h> bên C
+
 public class DogManagement {
 
     public static void main(String[] args) {
+        int         yob             =       2001;
+    // data type  tên gọi/biến          value, single, đơn, primitive
         
+        Dog         chiHu           = new Dog("CHI HU HU", 2021, 0.5);
+    // data type  tên biến object       phức tạp, object
+        
+        // có bao nhiêu data types? 2: primitive, object.
+        
+        chiHu.bark();
+        
+        Dog ngaoDa = new Dog("NGÁO ĐÁ", 2020, 50.0);
+        ngaoDa.bark();
+        
+        // Kiểm tra xem khi đổ vào Khuôn, đứa sau có đè đứa trước hay không?
+        System.out.println("CH barks again");
+        chiHu.bark();
+        
+        // 1 biến tại 1 thời điểm lưu 1 value.
+        int x = 68;
+        System.out.println("x: " + x);
+        x = 79;
+        System.out.println("x again: " + x);
     }
     
 }
