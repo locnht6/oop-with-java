5fb3b1ead0b2e07d7e2fb4f8f616d0650c1075b5
locnht6 <locnht6@gmail.com>
2026-06-13 13:09:07 +0700

    feat: update BasicArray project in Session05-Array

--- Session05-Array/BasicArray/src/basicarray/BasicArray.java
+++ Session05-Array/BasicArray/src/basicarray/BasicArray.java
@@ -4,7 +4,10 @@ public class BasicArray {
 
     public static void main(String[] args) {
         //playWithPrimitiveArray();
-        playWithPrimitiveArrayV2();
+        //playWithPrimitiveArrayV2();
+        //playWithPrimitiveArrayV3();
+        //playWithAnObject();
+        playWithObjectArray();
     }
     
     public static void playWithPrimitiveArray() {
@@ -76,6 +79,91 @@ public class BasicArray {
         // đây chính là đặc điểm của má mì, quản lí nhiều lính...
         // má mì ơi bà có bao nhiêu lính, má mì chấm length
         
+    }
+    
+    public static void playWithPrimitiveArrayV3() {
+        int a = 10;
+        // a chấm sổ cái đầu mày, a là 10 rõ ràng đòi gì nữa.
+        int a1 = 10, a2 = 100, a3, a4, a5, a6, a7, a8, a9, a11; // khai báo lẻ
+        System.out.println(a2 + " ");
+        
+        int arr1[] = {5,    10, 15, 20, 25, 30}; // có nhiều biến chung arr, implicit new
+        // cụ thể là arr[0] [1] [2] ...
+        
+        int arr2[] = new int[]{5, 10, 15, 20, 25, 30}; // explicit new
+        
+        int[] arr3 = {5, 10, 15, 20, 25, 30};
+        // đưa nhiều value thì phải có nhiều biến tương ứng,
+        // có nhiều biến lắm luôn và value tương ứng.
+        
+        int[] arr; // arr là biến má mì, con trỏ, trỏ vùng new cực bự, trỏ object chứa rất nhiều biến int khác.
+        arr = new int[30];
+        
+        System.out.println("The array arr has");
+        arr[0]= 100;
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        for (int x : arr) {
+            // x = arr[0] tao xin value thằng đầu tien.
+            // x = arr[1] tao xin value thằng kế, [i]
+            System.out.print(x + " ");          
+        }
+        System.out.println();
+        // arr[0] chấm xổ ra gì? không xổ ra gì, primitive xài luôn, chấm gì vì nó là 100.
+        
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+ 
+    public static void playWithAnObject() {
+        // Muốn lưu hồ sơ 1 SV, ta cần 2 thứ: biến, tên tắt, con trỏ, reference và vùng new clone đổ phễu.
+        // Muốn lưu 1 giá trị bình thường, ta cần 2 thứ: biến primitive và value đơn.
+        int a = 100;
+        Student x; // Mình cùng nói xấu đứa kia x đi.
+        // Đã là SV, thì chắc chắn showProfile(), công dân thì phải có CCCD.
+        x = new Student("SE123456", "AN NGUYỄN", 2001, 5.6);
+        
+        // hạ cánh nơi anh - cho con trỏ hạ cánh xuống 1 chỗ cực kì cool.
+        x = null; // trỏ vùng mới, hạ cánh đáy của ram,
+                  // gán địa chỉ 0, con số 0 vào trong biến con trỏ,
+                  // biến con trỏ lưu tọa độ đáy ram, chứa toàn số 0, byte null,
+                  // nhà đầu phố (số nhà 0) tối thui, không có gì,
+                  // nhà null.
+        x.showProfile();
+    }
+    
+    public static void playWithObjectArray() {
+        // Tôi muốn lưu hồ sơ 50 SV, tôi cần chuẩn bị những gì?
+        // 50 biến/tên tắt,
+        // 50 value - 50 vùng new
+        Student s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11, s12, s13; //...
+        //s1 = new Student(id, name, 0, 0)
+        
+        // 50 biến cái đã, mảng các biến SV.
+        Student s[] = new Student[50]; // 50 bạn SV, chưa có object thực
+                                       // s[0], s[1], s[2] ~~~ s1, s2, s3 ở khai báo lẻ.
+        s[0] = new Student("SE123456", "AN NGUYỄN", 2001, 5.6);
+        s[1] = new Student("SE123457", "BÌNH LÊ", 2000, 5.7);
+        s[2] = new Student("SE123458", "CƯỜNG VÕ", 2001, 5.8);
+        
+        System.out.println("The student list");
+//        s[0].showProfile();
+//        s[1].showProfile();
+//        s[2].showProfile();
+        
+        System.out.println("Cường lower: " + s[2].getName().toLowerCase().charAt(0));
+
+        for (int i = 0; i < 3; i++) {
+            s[i].showProfile();
+        }
+        
+        // Biến SC còn lại từ s[3]...s[49] giá trị default là gì?
+        // Default null hạ cánh đáy ram.
+        // Nguyên tắc chơi với mảng object cấm tuyệt đối for hết nếu mảng chưa gán full vì phần còn lại trỏ đáy ram mặc định,
+        // còn phần gán rồi thì chạy bình thường.
+        // Mảng object bắt buộc phải for đến count là số phần tử đã được gán.
     }
     
 }
