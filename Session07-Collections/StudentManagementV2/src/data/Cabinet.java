3d883b7eee486f34cbff7c3f23ea6179aaa96fdb
locnht6 <locnht6@gmail.com>
2026-06-17 22:38:25 +0700

    feat: update StudentManagementV2 project in Session07-Collections

--- Session07-Collections/StudentManagementV2/src/data/Cabinet.java
+++ Session07-Collections/StudentManagementV2/src/data/Cabinet.java
@@ -23,8 +23,21 @@ public class Cabinet {
         double gpa;
         
         System.out.println("Input student profile #" + (arr.size() + 1));
-        System.out.print("Input id: ");
-        id = sc.nextLine().toUpperCase().trim(); // trả về chuỗi cắt khoảng trắng dư đầu đuôi và đổi chuỗi vừa cắt qua HOA.
+        
+        while (true) {
+            System.out.print("Input id: ");
+            id = sc.nextLine().toUpperCase().trim(); // trả về chuỗi cắt khoảng trắng dư đầu đuôi và đổi chuỗi vừa cắt qua HOA.
+            // Vừa gõ id xong, check liền coi có trùng id không, có, chửi, bắt nhập lại.
+            // Còn sai còn ăn chửi, còn nhập lại.
+            Student xxx = searchAStudent(id);
+            if (xxx == null) {
+                break; // không thấy có SV nào ứng với id vừa gõ này, không trùng.
+                       // BREAK để nhập tiếp info còn lại.
+                       // Không trùng ID chính là khái niệm Primary Key bên Database.
+            }
+            else
+                System.out.println("Duplicated ID. Try with another one!!!");
+        }
         
         System.out.print("Input name: ");
         name = sc.nextLine().toUpperCase().trim();
@@ -52,5 +65,95 @@ public class Cabinet {
         }
         
     }
-
+    
+    // CRUD: Create/Retrieve(Read)/Update/Delete
+    // Viết hàm search là hàm quan trọng, vì nó không chỉ là search cho nhu cầu tìm hồ sơ,
+    // mà nó còn giúp cho các hành động khác: xóa, sửa hồ sơ.
+    // Tìm xem mã số SV vừa tạo mới có trùng không.
+    // tương đương 1 đăng kí 1 member, gõ acc báo trùng rồi, còn trùng còn không cho đi tiếp.
+    // Ta sẽ viết hàm search giải quyết được khá nhiều chuyện thì tốt hơn.
+    public Student searchAStudent(String id) {
+        
+        if (arr.isEmpty())
+            return null;
+        
+        for (int i = 0; i < arr.size(); i++) {
+            if (arr.get(i).getId().equalsIgnoreCase(id)) {
+                return arr.get(i);
+            }
+        }
+        
+        return null;
+        
+    }
+    
+    //  Overloading xảy ra
+    // Hàm này bao hàm trên lại - dùng lại, cung cấp id.
+    // Nhìn ra ngoài tủ ta thấy có 2 cách search, bạn đưa id trực tiếp hoặc bạn nhập id
+    public void searchAStudent() {
+        
+        String id;
+        System.out.print("Input a student id that you want to search: ");
+        id = sc.nextLine().trim();
+        Student xxx = searchAStudent(id);
+        if (xxx == null) {
+            System.out.println("NOT FOUND!!!");
+            return;
+        }
+        System.out.println("STUDENT FOUND! Here she/he is");
+        xxx.showProfile();
+        
+    }
+    
+    public void updateAStudent(String id, double newGPA) {
+        
+        Student xxx = searchAStudent(id);
+        
+        if (xxx == null)
+            System.out.println("STUDENT NOT FOUND! Nothing to update.");
+        else {
+            System.out.println("Before updating");
+            xxx.showProfile();
+            
+            xxx.setGpa(newGPA);
+            
+            System.out.println("After updating");
+            xxx.showProfile();
+        }
+        
+    }
+    
+    public void updateAStudent(Student x, double newGPA) {
+       
+        // Đã đưa tao tham chiếu đến SV x, thì tao set luôn.
+        if (x != null)
+            x.setGpa(newGPA);
+        
+    }
+    
+    public void updateAStudent() {
+        
+        String id;
+        double newGPA;
+        System.out.print("Input the student id that you want to update profile: ");
+        id = sc.nextLine().trim();
+        Student xxx = searchAStudent(id);
+        // Có tham chiếu trỏ đến vùng new Student() có id cần tìm.
+        if (xxx == null)
+            System.out.println("STUDENT NOT FOUND! Nothing to update.");
+        else {
+            System.out.println("Before updating");
+            xxx.showProfile();
+            
+            // Mời nhập điểm số mới/giá tiền mới.
+            System.out.print("Input new gpa: ");
+            newGPA = Double.parseDouble(sc.nextLine());
+            xxx.setGpa(newGPA);
+            
+            System.out.println("After updating");
+            xxx.showProfile();
+        }
+        
+    }
+    
 }
--- Session07-Collections/StudentManagementV2/src/studentmanagementv2/StudentManagementV2.java
+++ Session07-Collections/StudentManagementV2/src/studentmanagementv2/StudentManagementV2.java
@@ -4,23 +4,47 @@ import data.Cabinet;
 
 public class StudentManagementV2 {
 
+//    public static void main(String[] args) {
+//        
+//        Cabinet se = new Cabinet();
+//        Cabinet ia = new Cabinet();
+//        
+//        System.out.println("ADD SE");
+//        se.addAStudent(); // #1
+//        
+//        System.out.println("ADD IA");
+//        ia.addAStudent(); // #1
+//        ia.addAStudent(); // #2
+//        
+//        System.out.println("SE students");
+//        se.printStudentList(); // 1 SV
+//        
+//        System.out.println("IA students");
+//        ia.printStudentList(); // 2 SV
+//        
+//    }
+    
     public static void main(String[] args) {
         
-        Cabinet se = new Cabinet();
-        Cabinet ia = new Cabinet();
+        Cabinet se = new Cabinet(); // có cái Tủ, trong cái Tủ có cái Giỏ/Túi ~~~ ngăn trống
+        se.addAStudent();
+        se.addAStudent();
         
-        System.out.println("ADD SE");
-        se.addAStudent(); // #1
+        se.printStudentList();
         
-        System.out.println("ADD IA");
-        ia.addAStudent(); // #1
-        ia.addAStudent(); // #2
+        se.updateAStudent(); // giả bộ tìm không thấy SV
+        se.updateAStudent(); // gõ trúng id cần cập nhật
         
-        System.out.println("SE students");
-        se.printStudentList(); // 1 SV
+        System.out.println("Kiểm tra lại danh sách xem có đúng không");
+        se.printStudentList();
         
-        System.out.println("IA students");
-        ia.printStudentList(); // 2 SV
+        // Search tìm thấy
+        // String id; // Scanner...
+        // se.searchAStudent("SE123456") // hard-code hoặc nhập từ bàn phím ở main()
+        se.searchAStudent();
+        
+        // Search tìm không thấy
+        se.searchAStudent();
         
     }
     
